596a859f92a3b3c145914df0030b947a59e88d00
Christophe Vanfleteren <c.vanfleteren@gmail.com>
2013-06-30 00:48:49 +0200

    add simple async servlet

---
+++ fetcher/fetcher-web/src/main/scala/be/cvalue/crawler/fetcher/web/FetcherServlet.scala
@@ -0,0 +1,33 @@
+package be.cvalue.crawler.fetcher.web
+
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
+import be.cvalue.crawler.fetcher.api.UrlFetcher
+import scala.concurrent.{ExecutionContext, Await}
+import scala.concurrent.duration._
+import javax.servlet.annotation.WebServlet
+import ExecutionContext.Implicits.global
+
+@WebServlet(value = Array("/fetcha"), asyncSupported = true)
+class FetcherServlet(urlFetcher:UrlFetcher) extends HttpServlet {
+
+  override def doGet( req : HttpServletRequest, resp:HttpServletResponse ) : Unit =
+  {
+
+    val asyncContext = req.startAsync();
+    val f = urlFetcher.fetch("http://www.google.be");
+    f onSuccess {
+        case msg => {
+          resp.getWriter.write(msg)
+          asyncContext.complete()
+        }
+    }
+
+    f onFailure {
+      case _ => {
+        resp.getWriter.write("Error")
+        asyncContext.complete()
+      };
+    }
+  }
+
+}
