cdf5af42a3f75edcfb00b050a0abfb71798d9c7a
Christophe Vanfleteren <c.vanfleteren@gmail.com>
2013-07-10 23:45:17 +0200

    add bean validation

--- fetcher/fetcher-jaxrs/src/main/scala/be/cvalue/crawler/fetcher/jaxrs/UrlFetcherResource.scala
+++ fetcher/fetcher-jaxrs/src/main/scala/be/cvalue/crawler/fetcher/jaxrs/UrlFetcherResource.scala
@@ -2,11 +2,12 @@ package be.cvalue.crawler.fetcher.jaxrs
 
 import javax.ws.rs.{QueryParam, GET, Path}
 import javax.ws.rs.container.{AsyncResponse, Suspended}
+import javax.validation.constraints.{Size, NotNull}
 
 @Path("/hello")
 trait UrlFetcherResource {
 
   @GET
-  def fetch(@QueryParam("url") url:String, @Suspended asyncResponse:AsyncResponse) : Unit
+  def fetch(@NotNull @Size(min=5) @QueryParam("url") url:String, @Suspended asyncResponse:AsyncResponse) : Unit
 
 }
